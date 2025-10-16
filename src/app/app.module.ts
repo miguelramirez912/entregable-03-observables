2f6926a3150e11d5b3229f684270e78c0d550ce9
Miguel Angel Ramirez Herrera <miguel.ramirezh@infotec.mx>
2025-10-16 13:03:08 -0600

    Configuradas las rutas de la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+import { GastosComponent } from './components/gastos/gastos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IngresarPresupuestoComponent
+    IngresarPresupuestoComponent,
+    GastosComponent
   ],
   imports: [
     BrowserModule,
