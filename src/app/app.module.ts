4c1a7a85ac6cf6ed3612ce97d84e7dbf81417245
Miguel Angel Ramirez Herrera <miguel.ramirezh@infotec.mx>
2025-10-16 12:39:20 -0600

    Creados los componentes y el servicio necesario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngresarPresupuestoComponent
   ],
   imports: [
     BrowserModule,
