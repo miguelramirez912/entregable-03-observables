acddb57b9036e3f4bc5dbb36a76b8288f7e1a551
Miguel Ramirez <miguelramirez88@hotmail.com>
2025-10-17 01:22:58 -0600

    Se crea el HTML del componente IngresarGasto y la función agregarGasto para mostrar el bagde de error

--- src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
+++ src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ingresar-gasto',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./ingresar-gasto.component.css']
 })
 export class IngresarGastoComponent {
+  nombreGasto: string;
+  cantidad: number;
+  formularioIncorrecto: boolean;
+  textoIncorrecto: string;
+
+  constructor(){
+    this.nombreGasto = '';
+    this.cantidad = 0;
+    this.formularioIncorrecto = false;
+    this.textoIncorrecto = 'El nombre del gasto o la cantidad son incorrectos'
+  }
+
+  agregarGasto(){
+    if (this.nombreGasto === '' || this.cantidad <= 0) {
+      this.formularioIncorrecto = true;
+    } else {
+      this.formularioIncorrecto = false;
+      this.nombreGasto = '';
+      this.cantidad = 0;
+    }
+  }
 
 }
