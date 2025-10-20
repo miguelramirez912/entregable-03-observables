286fe1c8246a89de8aa3479459e4eb75c3a6f841
Miguel Angel Ramirez Herrera <miguel.ramirezh@infotec.mx>
2025-10-20 14:19:11 -0600

    Se hace Observable a presupuesto service. Se suscribe al servicio al Componente ListarGasto

--- src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-listar-gasto',
   templateUrl: './listar-gasto.component.html',
   styleUrls: ['./listar-gasto.component.css']
 })
-export class ListarGastoComponent {
+export class ListarGastoComponent implements OnDestroy {
+  subscripcion: Subscription;
+
+  constructor(private _presupuestoService: PresupuestoService){
+    this.subscripcion = this._presupuestoService.getGastos().subscribe(data => {
+      console.log(data)
+    })
+  }
+  ngOnDestroy(): void {
+    this.subscripcion.unsubscribe;
+  }
 
 }
