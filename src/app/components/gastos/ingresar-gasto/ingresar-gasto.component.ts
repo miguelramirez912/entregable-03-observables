ac9f55ca78d60cc2871107954bfcd8e31cd11e5c
Miguel Angel Ramirez Herrera <miguel.ramirezh@infotec.mx>
2025-10-20 13:28:45 -0600

    Implementada las funciones agregar del componente IngresarGasto y agregarGasto del servicio presupuesto service

--- src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
+++ src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
   selector: 'app-ingresar-gasto',
@@ -11,22 +12,46 @@ export class IngresarGastoComponent {
   cantidad: number;
   formularioIncorrecto: boolean;
   textoIncorrecto: string;
+  restante: number;
 
-  constructor(){
+  constructor(private _presupuestoService: PresupuestoService){
     this.nombreGasto = '';
     this.cantidad = 0;
     this.formularioIncorrecto = false;
-    this.textoIncorrecto = 'El nombre del gasto o la cantidad son incorrectos'
+    this.textoIncorrecto = 'El nombre del gasto o la cantidad son incorrectos';
+    this.restante = _presupuestoService.restante;
   }
 
-  agregarGasto(){
+  
+  agregar(){
+    
+    if (this.cantidad > this._presupuestoService.restante) {
+      this.formularioIncorrecto = true;
+      this.textoIncorrecto = 'La cantidad ingresada es mayor a la restante'
+      return;
+    }
+
     if (this.nombreGasto === '' || this.cantidad <= 0) {
       this.formularioIncorrecto = true;
+      this.textoIncorrecto = 'El nombre del gasto o la cantidad son incorrectos'
     } else {
-      this.formularioIncorrecto = false;
-      this.nombreGasto = '';
-      this.cantidad = 0;
+
+      // Creamos el Objeto Gasto
+      const GASTO = {
+        nombre: this.nombreGasto,
+        cantidad: this.cantidad
+      }
+
+      // Enviamos el Objeto a los suscriptores via subject
+      this._presupuestoService.agregarGasto(GASTO);
+
+      // Reseteamos el formulario
+          this.formularioIncorrecto = false;
+          this.nombreGasto = '';
+          this.cantidad = 0;
     }
   }
+
+  
 
 }
