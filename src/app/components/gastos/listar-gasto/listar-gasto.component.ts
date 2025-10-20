b871eede5624dcd7f3f26f02adab249e9ef2370c
Miguel Angel Ramirez Herrera <miguel.ramirezh@infotec.mx>
2025-10-20 17:01:25 -0600

    Se crea el HTML del componente ListarGasto. Se crea e itera sobre la lista listGastos para mostrar en la UI todos los gastos

--- src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PresupuestoService } from 'src/app/services/presupuesto.service';
 import { Subscription } from 'rxjs';
 
@@ -7,16 +7,40 @@ import { Subscription } from 'rxjs';
   templateUrl: './listar-gasto.component.html',
   styleUrls: ['./listar-gasto.component.css']
 })
-export class ListarGastoComponent implements OnDestroy {
+export class ListarGastoComponent implements OnInit, OnDestroy {
   subscripcion: Subscription;
+  presupuesto: number;
+  restante: number;
+  listGastos: any[] = []
 
   constructor(private _presupuestoService: PresupuestoService){
+    this.presupuesto = 0;
+    this.restante = 0;
     this.subscripcion = this._presupuestoService.getGastos().subscribe(data => {
       console.log(data)
+      this.restante = this.restante - data.cantidad;
+      this.listGastos.push(data)
     })
   }
+
+  ngOnInit(): void {
+    this.presupuesto = this._presupuestoService.presupuesto;
+    this.restante = this._presupuestoService.restante;
+    console.log(this.listGastos);
+  }
+
   ngOnDestroy(): void {
     this.subscripcion.unsubscribe;
+  }
+
+  aplicarColorRestante(){
+    if (this.presupuesto / 4 > this.restante) {
+      return 'alert alert-danger';
+    } else if (this.presupuesto / 2 > this.restante) {
+      return 'alert alert-warning';
+    } else {
+      return 'alert alert-secondary';
+    }
   }
 
 }
